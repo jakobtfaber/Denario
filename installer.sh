3551a560bcde1ab98f59934153e506836967fad7
borisbolliet <boris.bolliet@gmail.com>
2025-04-11 13:30:23 +0200

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+# installer.sh
+# This script sets up a Python virtual environment, upgrades pip,
+# and clones & installs three Git repositories using specific branches.
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# Function to clone a repo if it doesn't exist
+clone_repo() {
+  local repo_url=$1
+  local repo_dir=$2
+  if [ ! -d "$repo_dir" ]; then
+    echo "Cloning $repo_url into $repo_dir ..."
+    git clone "$repo_url"
+  else
+    echo "Directory $repo_dir already exists. Skipping clone."
+  fi
+}
+
+# Create a Python 3.12 virtual environment named 'astrop_env'
+echo "Creating Python virtual environment..."
+python3.12 -m venv astrop_env
+
+# Activate the virtual environment
+echo "Activating virtual environment..."
+source astrop_env/bin/activate
+
+# Upgrade pip
+echo "Upgrading pip..."
+python -m pip install --upgrade pip
+
+### Clone and install AstroPilot ###
+ASTROPILOT_REPO="https://github.com/AstroPilot-AI/AstroPilot.git"
+ASTROPILOT_DIR="AstroPilot"
+
+clone_repo "$ASTROPILOT_REPO" "$ASTROPILOT_DIR"
+
+echo "Switching to branch 'bbdev' and installing AstroPilot..."
+cd "$ASTROPILOT_DIR"
+git switch bbdev
+pip install -e .
+cd ..
+
+### Clone and install ag2 ###
+AG2_REPO="https://github.com/CMBAgents/ag2.git"
+AG2_DIR="ag2"
+
+clone_repo "$AG2_REPO" "$AG2_DIR"
+
+echo "Switching to branch 'ag2_v0.8.4_upgrade_astrop' and installing ag2..."
+cd "$AG2_DIR"
+git switch ag2_v0.8.4_upgrade_astrop
+pip install -e .
+cd ..
+
+### Clone and install cmbagent ###
+CMBAGENT_REPO="https://github.com/CMBAgents/cmbagent.git"
+CMBAGENT_DIR="cmbagent"
+
+clone_repo "$CMBAGENT_REPO" "$CMBAGENT_DIR"
+
+echo "Switching to branch 'astrop' and installing cmbagent..."
+cd "$CMBAGENT_DIR"
+git switch astrop
+pip install -e .
+cd ..
+
+echo "Installation complete!"
