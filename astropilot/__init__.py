22f7aef8ab97aeea923ac95ddba872d0c2e89f0c
franciscovillaescusa <villaescusa.francisco@gmail.com>
2025-05-07 20:19:50 -0400

    added centralized LLM dictionary to be passed as argument to astropilot functions

--- astropilot/__init__.py
+++ astropilot/__init__.py
@@ -1,5 +1,5 @@
-from .astro_pilot import AstroPilot, Research, Journal
+from .astro_pilot import AstroPilot, Research, Journal, LLM
 from .config import REPO_DIR
 
-__all__ = ['AstroPilot', 'Research', 'Journal', 'REPO_DIR']
+__all__ = ['AstroPilot', 'Research', 'Journal', 'REPO_DIR', 'LLM']
 
