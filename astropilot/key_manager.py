92bd9c18c59b6698b676a0441928f6e12b47bace
Pablo Villanueva Domingo <pablo.villanueva.domingo@gmail.com>
2025-05-08 09:55:45 +0200

    Add key manager

---
+++ astropilot/key_manager.py
@@ -0,0 +1,24 @@
+import os
+from pydantic import BaseModel
+from dotenv import load_dotenv
+
+class KeyManager(BaseModel):
+    ANTHROPIC: str = ""
+    GEMINI: str = ""
+    OPENAI: str = ""
+    PERPLEXITY: str = ""
+
+    def get_keys_from_env(self) -> None:
+
+        self.ANTHROPIC      = os.getenv("ANTHROPIC_API_KEY")
+        self.GEMINI         = os.getenv("GOOGLE_API_KEY")
+        self.PERPLEXITY     = os.getenv("PERPLEXITY_API_KEY")
+        self.OPENAI         = os.getenv("OPENAI_API_KEY")
+
+    def get_keys_from_dotenv(self) -> None:
+
+        load_dotenv()
+
+        self.get_keys_from_env()
+
+        
