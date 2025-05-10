c0cb6b328934b19e9b01afe0042a96c11913d490
Pablo Villanueva Domingo <pablo.villanueva.domingo@gmail.com>
2025-05-10 03:28:20 +0200

    Create LLM class

--- astropilot/llm.py
+++ astropilot/llm.py
@@ -1,31 +1,56 @@
-LLM = {
-    "gemini-2.0-flash":
-    {"name": "gemini-2.0-flash", "max_output_tokens": 8192, "temperature":0.7},
-       
-    "gemini-2.5-flash":
-    {"name": "gemini-2.5-flash-preview-04-17", "max_output_tokens": 65536, "temperature":0.7},
-    
-    "gemini-2.5-pro":
-    {"name": "gemini-2.5-pro-preview-05-06", "max_output_tokens": 65536, "temperature":0.7},
-    
-    "o3-mini-2025-01-31":
-    {"name": "o3-mini-2025-01-31", "max_output_tokens": 100000, "temperature":None},
+from pydantic import BaseModel
+from typing import ClassVar, Dict
 
-    "gpt-4o":
-    {"name": "gpt-4o-2024-11-20", "max_output_tokens":16384, "temperature":0.5},
+class LLM(BaseModel):
+    name: str
+    max_output_tokens: int
+    temperature: float | None
 
-    'gpt-4.1':
-    {"name": "gpt-4.1-2025-04-14", "max_output_tokens": 16384, "temperature":0.5},
+gemini20flash = LLM(name="gemini-2.0-flash",
+                    max_output_tokens=8192,
+                    temperature=0.7)
 
-    'gpt-4o-mini':
-    {"name": "gpt-4o-mini-2024-07-18", "max_output_tokens": 16384, "temperature":0.5},
+gemini25flash = LLM(name="gemini-2.5-flash-preview-04-17",
+                    max_output_tokens=65536,
+                    temperature=0.7)
 
-    "gpt-4.5":
-    {"name": "gpt-4.5-preview-2025-02-27", "max_output_tokens": 16384, "temperature":0.5},
-       
-    'claude-3-7-sonnet':
-    {"name":'claude-3-7-sonnet-20250219', "max_output_tokens": 64000, "temperature":0},
+gemini25pro = LLM(name="gemini-2.5-pro-preview-05-06",
+                  max_output_tokens=65536,
+                  temperature=0.7)
 
+o3mini = LLM(name="o3-mini-2025-01-31",
+             max_output_tokens=100000,
+             temperature=None)
 
-}
-    
+gpt4o = LLM(name="gpt-4o-2024-11-20",
+            max_output_tokens=16384,
+            temperature=0.5)
+
+gpt41 = LLM(name="gpt-4.1-2025-04-14",
+            max_output_tokens=16384,
+            temperature=0.5)
+
+gpt4omini = LLM(name="gpt-4o-mini-2024-07-18",
+                max_output_tokens=16384,
+                temperature=0.5)
+
+gpt45 = LLM(name="gpt-4.5-preview-2025-02-27",
+            max_output_tokens=16384,
+            temperature=0.5)
+
+claude37sonnet = LLM(name="claude-3-7-sonnet-20250219",
+                     max_output_tokens=64000,
+                     temperature=0)
+            
+
+models : Dict[str, LLM] = {
+                            "gemini-2.0-flash" : gemini20flash,
+                            "gemini-2.5-flash" : gemini25flash,
+                            "gemini-2.5-pro" : gemini25pro,
+                            "o3-mini" : o3mini,
+                            "gpt-4o" : gpt4o,
+                            "gpt-4.1" : gpt41,
+                            "gpt-4o-mini" : gpt4omini,
+                            "gpt-4.5" : gpt45,
+                            "claude-3.7-sonnet" : claude37sonnet,
+                           }
