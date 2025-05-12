fb95dc37a6f14d1981066aa389d7ad535b017884
borisbolliet <boris.bolliet@gmail.com>
2025-05-13 01:12:58 +0200

    extends the idea description

--- astropilot/idea.py
+++ astropilot/idea.py
@@ -17,7 +17,7 @@ class Idea:
     - Ask `idea_maker` to select and improve 2 out of the 5 research project ideas given the output of the `idea_hater`.
     - Ask `idea_hater` to critique the 2 improved ideas. 
     - Ask `idea_maker` to select the best idea out of the 2. 
-    - Ask `idea_maker` to report the best idea in the form of a scientific paper title with a 1 sentence description. 
+    - Ask `idea_maker` to report the best idea in the form of a scientific paper title with a 5-sentence description. 
 
     Args:
         work_dir: working directory.
@@ -42,7 +42,7 @@ class Idea:
         - Ask idea_maker to select and improve 2 out of the 5 research project ideas given the output of the idea_hater.
         - Ask idea_hater to critique the 2 improved ideas. 
         - Ask idea_maker to select the best idea out of the 2. 
-        - Ask idea_maker to report the best idea in the form of a scientific paper title with a 1 sentence description. 
+        - Ask idea_maker to report the best idea in the form of a scientific paper title with a 5-sentence description. 
 
         The goal of this task is to generate a research project idea based on the data of interest. 
         Don't suggest to perform any calculations or analyses here. The only goal of this task is to obtain the best possible project idea.
