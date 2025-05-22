d6a2e981bbb4ac0ebaad473fccda1401a3c6ea12
Pablo Villanueva Domingo <pablo.villanueva.domingo@gmail.com>
2025-05-23 00:04:55 +0200

    Allow to access to KeyManager as a dict

--- astropilot/key_manager.py
+++ astropilot/key_manager.py
@@ -16,3 +16,9 @@ class KeyManager(BaseModel):
         self.GEMINI         = os.getenv("GOOGLE_API_KEY")
         self.PERPLEXITY     = os.getenv("PERPLEXITY_API_KEY")
         self.OPENAI         = os.getenv("OPENAI_API_KEY")
+
+    def __getitem__(self, key: str) -> str:
+        return getattr(self, key)
+
+    def __setitem__(self, key: str, value: str) -> None:
+        setattr(self, key, value)
--- astropilot/paper_agents/literature.py
+++ astropilot/paper_agents/literature.py
@@ -1,5 +1,4 @@
 import re
-import os
 import requests
 from typing import List, Tuple
 
