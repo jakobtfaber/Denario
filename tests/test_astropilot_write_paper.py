9c6d3023dca8f7d7e8a96829dd5020d0c2e408c0
Pablo Villanueva Domingo <pablo.villanueva.domingo@gmail.com>
2025-06-04 01:01:52 +0200

    Additional minor modifications

--- tests/test_astropilot_write_paper.py
+++
@@ -1,9 +0,0 @@
-import os
-os.environ["CMBAGENT_DEBUG"] = "false"
-os.environ["ASTROPILOT_DISABLE_DISPLAY"] = "false"
-from denario import Denario
-
-params = {}
-
-den = Denario(params)
-den.get_paper()
