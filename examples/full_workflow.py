9e717a1eee2e421c17bea0d1a353a720279f66db
Pablo Villanueva Domingo <pablo.villanueva.domingo@gmail.com>
2025-10-21 18:02:17 +0200

    Remove GW data and add function to example to download it

--- examples/full_workflow.py
+++ examples/full_workflow.py
@@ -1,18 +1,38 @@
 # This script runs the full workflow of Denario.
 # and allows specifying the LLM models to use.
+# It makes use of gravitational waves data from a binary black hole merger
+# This data has been preprocessed by Jay Wadekar
+# Original data can be found at https://zenodo.org/records/16004263
 # The 'mini' models are much cheaper than the 'pro' models.
 # but their output quality can be slightly lower.
 # see e.g., https://platform.openai.com/docs/pricing
 
 from denario import Denario, Journal
 
+import os
+import urllib.request
+
+# Function to download GW data, already preprocessed
+def download_data(workdir):
+    os.makedirs(f"{workdir}/data", exist_ok=True)
+    csv_files = ["GW231123_IMRPhenomXO4a.csv","GW231123_NRSur7dq4.csv"]
+    for file in csv_files:
+        outfile = f"{workdir}/data/{file}"
+        if not os.path.isfile(outfile):
+            print("Downloading data to",outfile)
+            url = f"https://users.flatironinstitute.org/~fvillaescusa/Denario/Astrophysics/GW/data/{file}"
+            urllib.request.urlretrieve(url, outfile)
+
 # This is one of the example projects in the examples folder.
 # It investigates the properties of a recent gravitational wave detection
 # called GW231123 by the LIGO and Virgo detectors (https://arxiv.org/pdf/2507.08219)
 # The data description is in the input.md file.
 # All the outputs are saved in the same folder.
-folder = "GW231123"
-astro_pilot = Denario(project_dir=folder)
+project_dir = "GW231123"
+astro_pilot = Denario(project_dir=project_dir)
+
+# Download the data if already does not exist
+download_data(project_dir)
 
 # Set the input prompt containing the data description
 # WARNING: PLEASE PROVIDE ABSOLUTE PATHS to all the data files listed in the .md file
